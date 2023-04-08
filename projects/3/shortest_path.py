92f03596d5658ffff4e3094c9cda2fab31ca0d80
SemyonSavoskin <s.v.savoslin@gmail.com>
2023-04-08 13:18:49 +0000

    2

--- projects/3/shortest_path.py
+++ projects/3/shortest_path.py
@@ -43,7 +43,7 @@ edges = data.map(lambda x: tuple(x.split('\t')))
 adjList = edges.groupByKey().cache()
 
 
-maxPathLength = 11
+maxPathLength = 20
 
 bfs = adjList.mapValues(lambda x: initializeBFS(startNode) if x == startNode else ([], float('inf')))
 
