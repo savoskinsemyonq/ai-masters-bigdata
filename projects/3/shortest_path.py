970fb13a4453d97a1948c8fbc691322da208cf6f
SemyonSavoskin <s.v.savoslin@gmail.com>
2023-04-09 09:42:26 +0000

    done 3task

--- projects/3/shortest_path.py
+++ projects/3/shortest_path.py
@@ -1,61 +1,68 @@
-#!/opt/conda/envs/dsenv/bin/python 
-from pyspark import SparkConf, SparkContext
+#!/opt/conda/envs/dsenv/bin/python
+import os
 import sys
-import logging
 
-def initializeBFS(startNode):
-    return (startNode, ([startNode], 0))
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+from pyspark import SparkContext
+from pyspark.sql.types import *
 
-def updateBFS(node, bfsData, targetNode):
-    path, dist = bfsData
-    newpath = list(path)
-    newpath.append(node)
-    if node == targetNode:
-        return (newpath, dist + 1)
-    else:
-        return (newpath, dist)
+SPARK_HOME = "/usr/lib/spark3"
+PYSPARK_PYTHON = "/opt/conda/envs/dsenv/bin/python"
+PYSPARK_DRIVER_PYTHON = "/opt/conda/envs/dsenv/bin/python"
+os.environ["PYSPARK_PYTHON"]= PYSPARK_PYTHON
+os.environ["PYSPARK_DRIVER_PYTHON"]= PYSPARK_DRIVER_PYTHON
+os.environ["SPARK_HOME"] = SPARK_HOME
 
-def mergeBFS(bfsData1, bfsData2):
-    path1, dist1 = bfsData1
-    path2, dist2 = bfsData2
-    if dist1 < dist2:
-        return bfsData1
-    elif dist1 > dist2:
-        return bfsData2
-    else:
-        return (path1 + path2, dist1)
+PYSPARK_HOME = os.path.join(SPARK_HOME, "python/lib")
+sys.path.insert(0, os.path.join(PYSPARK_HOME, "py4j-0.10.9.3-src.zip"))
+sys.path.insert(0, os.path.join(PYSPARK_HOME, "pyspark.zip"))
 
+start = sys.argv[1] 
+finish = sys.argv[2]
+input = sys.argv[3] 
+output = sys.argv[4]
 
-startNode = sys.argv[1]
-endNode = sys.argv[2]
-inputPath = sys.argv[3]
-outputPath = sys.argv[4]
+sc = SparkContext()
+conf = SparkConf()
+spark = SparkSession.builder.config(conf=conf).appName("ShortestPath").getOrCreate()
 
-conf = SparkConf().setAppName("ShortestPath")
-sc = SparkContext(conf=conf)
+schema = StructType(fields=[
+    StructField("user_id", IntegerType()),
+    StructField("follower_id", IntegerType())])
 
+df = spark.read\
+          .schema(schema)\
+          .format("csv")\
+          .option("sep", "\t")\
+          .load(input)
 
-data = sc.textFile(inputPath)
+def bfs(graph, start, end):
+    queue = []
+    queue.append([[start],0])
+    res = []
+    level_end = -1
+    while queue:
+        path_level = queue.pop(0)
+        path = path_level[0]
+        level = path_level[1]
+        if level_end != -1 and level > level_end - 1:
+            break
+        node = path[-1]
+        users_of_node = graph.filter(graph.follower_id == node).select('user_id').rdd.toLocalIterator()
+        for adjacent in users_of_node:
+            new_path = list(path)
+            new_path.append(adjacent.user_id)
+            queue.append([new_path,level+1])
+            
+            if adjacent.user_id == end:
+                res.append(new_path)
+                level_end = level+1
+            else:
+                continue
+    return res
 
-edges = data.map(lambda x: tuple(x.split('\t')))
-
-# print(edges)
-adjList = edges.groupByKey().cache()
-
-
-maxPathLength = 100
-
-bfs = adjList.mapValues(lambda x: initializeBFS(startNode) if x == startNode else ([], float('inf')))
-
-for i in range(maxPathLength):
-    
-    bfs = bfs.flatMapValues(lambda x: x[0]).map(lambda x: (x[1], x[0])).join(adjList).map(lambda x: (x[1][0], x[0])).reduceByKey(lambda x, y: mergeBFS(x, y))
-
-    bfs = bfs.flatMapValues(lambda x: x[0]).map(lambda x: (x[1], x[0])).join(adjList).map(lambda x: (x[1][0], updateBFS(x[1][1], x[0], endNode)))
-    
-    shortestPath = bfs.filter(lambda x: x[0] == endNode and x[1][1] <= i+1).collect()
-    if shortestPath:
-        break
-# logging.info("Path {}".format(shortestPath))
-
-sc.parallelize([12,422,53,52,107,20,23,274,34]).saveAsTextFile(outputPath)
+rep = bfs(df, start, finish)
+df_rep = spark.createDataFrame(data=rep)
+df_rep.write.csv(output, sep=',')
+spark.stop()
