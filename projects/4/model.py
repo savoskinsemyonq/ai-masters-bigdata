3ad46d8a804a88e99cc3f2c5f1bde22b5d7cbc32
SemyonSavoskin <s.v.savoslin@gmail.com>
2023-04-15 17:36:32 +0000

    4

--- projects/4/model.py
+++ projects/4/model.py
@@ -2,18 +2,17 @@
 
 from pyspark.sql.types import *
 from pyspark.ml.feature import *
-from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.regression import LinearRegression
 from pyspark.ml import Pipeline
 
 
-tokenizer = Tokenizer(inputCol="reviewText", outputCol="text")
-hasher = HashingTF(numFeatures=100, binary=True, inputCol=tokenizer.getOutputCol(), outputCol="word_vector")
-assembler = VectorAssembler(inputCols=[hasher.getOutputCol(), "comment_length", "vote"], outputCol="features")
-lr = LogisticRegression(labelCol="overall", maxIter=25)
+tokenizer = Tokenizer(inputCol="reviewText", outputCol="words")
+hasher = HashingTF(numFeatures=5000, binary=True, inputCol=tokenizer.getOutputCol(), outputCol="word_vector")
+lr = LinearRegression(featuresCol=hasher.getOutputCol(), labelCol="overall", maxIter=50)
 
 pipeline = Pipeline(stages=[
     tokenizer,
     hasher,
-    assembler,
     lr
 ])
